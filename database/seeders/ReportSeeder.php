b099ea1aaa0c84c576891c8e86b2de6016d22461
Rio-Rf <iwata.rio.1016@gmail.com>
2023-08-24 19:50:14 +0900

    [fix]user_loginの修正

--- database/seeders/ReportSeeder.php
+++ database/seeders/ReportSeeder.php
@@ -14,11 +14,125 @@ class ReportSeeder extends Seeder
      *
      * @return void
      */
+    /*
     public function run()
     {
         DB::table('reports')->insert([
             'id' => 999,
             'user_id' => 1,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+    }*/
+    public function run()
+    {
+        DB::table('reports')->insert([
+            'id' => 1,
+            'user_id' => 1,
+            'title' => 'aaa',
+            'content' => 'zzz',
+            'address' => 'ccc',
+            'lat' => 36.3,
+            'lng' => 140.55,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reports')->insert([
+            'id' => 2,
+            'user_id' => 1,
+            'title' => 'aaa',
+            'content' => 'zzzaaa',
+            'address' => 'ccc',
+            'lat' => 36.3,
+            'lng' => 140.55,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reports')->insert([
+            'id' => 3,
+            'user_id' => 1,
+            'title' => 'aaa',
+            'content' => 'zzzddd',
+            'address' => 'ccc',
+            'lat' => 36.3,
+            'lng' => 140.55,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reports')->insert([
+            'id' => 4,
+            'user_id' => 1,
+            'title' => 'aaa',
+            'content' => 'zzzmmm',
+            'address' => 'ccc',
+            'lat' => 36.3,
+            'lng' => 140.55,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reports')->insert([
+            'id' => 5,
+            'user_id' => 1,
+            'title' => 'aaa',
+            'content' => 'zzz',
+            'address' => 'ccsaaac',
+            'lat' => 36.3,
+            'lng' => 140.55,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reports')->insert([
+            'id' => 6,
+            'user_id' => 1,
+            'title' => 'aaaeea',
+            'content' => 'zzz',
+            'address' => 'ccc',
+            'lat' => 36.3,
+            'lng' => 140.55,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reports')->insert([
+            'id' => 7,
+            'user_id' => 1,
+            'title' => 'faaa',
+            'content' => 'zfzz',
+            'address' => 'cfcc',
+            'lat' => 36.3,
+            'lng' => 140.55,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reports')->insert([
+            'id' => 8,
+            'user_id' => 1,
+            'title' => 'taaa',
+            'content' => 'ztzz',
+            'address' => 'cctc',
+            'lat' => 36.3,
+            'lng' => 140.55,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reports')->insert([
+            'id' => 9,
+            'user_id' => 1,
+            'title' => 'aaya',
+            'content' => 'zzyz',
+            'address' => 'cycc',
+            'lat' => 36.3,
+            'lng' => 140.55,
+            'created_at' => new DateTime(),
+            'updated_at' => new DateTime(),
+        ]);
+        DB::table('reports')->insert([
+            'id' => 10,
+            'user_id' => 1,
+            'title' => 'aoaa',
+            'content' => 'ozzz',
+            'address' => 'ccoc',
+            'lat' => 36.3,
+            'lng' => 140.55,
             'created_at' => new DateTime(),
             'updated_at' => new DateTime(),
         ]);
